a2e1c2823c6a4f73e36849ac786ce53608f43f6b
Tanish <tanish.singla@anywhere.co>
2025-12-08 18:11:42 +0530

    Button task

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import { useState, memo } from "react";
+import "./App.css";
 
-const App = () => {
-  return <div>App</div>;
-};
+const Button = memo(({ currentIndex, setIndex, isActive }) => {
+  console.log("rendered");
+  const handleButtonClick = () => {
+    setIndex(currentIndex);
+  };
+
+  return (
+    <button
+      onClick={handleButtonClick}
+      style={{
+        color: isActive ? "green" : "red",
+      }}
+    >
+      Click {currentIndex}
+    </button>
+  );
+});
+
+function App() {
+  const [index, setIndex] = useState(null);
+  const items = 5;
+
+  return (
+    <>
+      {Array.from({ length: items }).map((_, i) => {
+        return (
+          <Button
+            key={i}
+            currentIndex={i}
+            setIndex={setIndex}
+            isActive={index === i}
+          />
+        );
+      })}
+    </>
+  );
+}
 
 export default App;
